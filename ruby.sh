8c17df0bcec1d8037937cd83c45a511f86453927
Steve Laskaridis <stevelaskaridis@gmail.com>
2016-03-31 00:45:51 +0200

    FIx inexistent rvm_path env var

--- ruby.sh
+++ ruby.sh
@@ -6,6 +6,7 @@
 
 # RVM
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+source $HOME/.rvm/scripts/rvm
 
 # RVM completion
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
